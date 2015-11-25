9316f343c3b8faa40744cc03b17dfd90061812cc
EnlightenDev <enlightendevio@gmail.com>
2015-11-25 12:15:01 -0500

    misc

--- src/main/java/io/enlightendev/AwsRefSpringbootApplication.java
+++ src/main/java/io/enlightendev/AwsRefSpringbootApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-class AwsRefSpringbootApplication {
+public class AwsRefSpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AwsRefSpringbootApplication.class, args);
